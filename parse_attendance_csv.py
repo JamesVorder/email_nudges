82d8243f22e1e42078e8f7ecf1759da6fb695d3c
JamesVorder <james@bip44.email>
2019-07-15 19:02:25 -0400

    Extract students straight away.

--- parse_attendance_csv.py
+++ parse_attendance_csv.py
@@ -58,8 +58,25 @@ def filter_subtotals(lines_in, _out, subtotals_ranges):
             cursor = tup[1]
     return output
 
+class Student: 
+    def __init__(self, grade, ID, name, days_enrolled, days_not_enrolled, days_present, days_excused, days_not_excused):
+        self.name = name
+        self.grade = grade
+    def __str__(self):
+        return self.name
+
+def extract_students(_in):
+    rows = csv.reader(_in)
+    curr_grade = "" 
+    output = []
+    for row in rows:
+        if re.search("Grade Level:.*$", row[0]):
+            curr_grade = re.search("(\d+)", row[0]).groups()[0]
+        elif re.search("\d{6}.*$", row[0]):
+            output.append(Student(curr_grade, row[0], row[2], row[6], row[7], row[8], row[9], row[10])) 
+    #print(output[0].grade)
+    return output
+
 with open('test_data/test.csv', 'r') as test_input:
-   test_lines = test_input.readlines()
-   # locate_students(test_lines)
-   locate_grade(test_lines, "09")
+   extract_students(test_input)
 
