fb6ef470aa85a3a9cdbf8f80bbcc54d554295a5e
James Vorderbruggen <james@bip44.email>
2019-07-30 14:26:28 -0400

    Restructure parser.

--- app.py
+++ app.py
@@ -11,27 +11,28 @@
 
 from tkinter import *
 from tkinter.filedialog import askopenfilename
-import parse_attendance_csv
+import parse_attendance_csv as parser
 
 class App:
     def __init__(self, master):
- 
+
         master.title("Attendance Nudge-er")
 
         self.btn_quit = Button(master, text="QUIT", fg="red", command=master.quit)
         self.btn_quit.grid(row=0, column=0)
 
         self.btn_pick = Button(master, text="Import Report", fg="green", command=self.import_report)
-        self.btn_pick.grid(row=0, column=1) 
+        self.btn_pick.grid(row=0, column=1)
 
         self.lbl_out = Label(master, text="No reports run...")
-        self.lbl_out.grid(row=1, columnspan=2) 
+        self.lbl_out.grid(row=1, columnspan=2)
 
     def import_report(self):
         filename = askopenfilename()
-        print(filename)
-        parse_attendance_csv.import_attendance_from_csv(filename)
-        self.lbl_out.config(text="Ran a report!")
+        print(f'Reading {filename}')
+        report = parser.AttendanceReport(filename, target_grade="09", db="attendance_nudger_v1")
+        report.read()
+        self.lbl_out.config(text="Imported a report!")
 
 ### MAIN ###
 root = Tk()
--- parse_attendance_csv.py
+++ parse_attendance_csv.py
@@ -53,77 +53,84 @@ class Student:
     def get_class_average_attendance(self):
         return abs(self.attendance_distance - self.attendance_rate)
 
-    def add_to_db(self, cursor): 
-        cursor.execute("CREATE TABLE if not exists students (ID integer, name text, grade integer)") 
-        cursor.execute("INSERT INTO students VALUES(?, ?, ?)", (int(self.ID), self.name, int(self.grade))) 
-        return cursor
+    def add_to_db(self, connection):
+        connection.execute("CREATE TABLE if not exists students (ID integer, name text, grade integer)")
+        connection.execute("INSERT INTO students VALUES(?, ?, ?)", (int(self.ID), self.name, int(self.grade)))
+        connection.commit()
 
-def import_attendance_from_csv(filename):
+class AttendanceReport:
+    def __init__(self, filename, db, target_grade="09"):
+        self.connection = sqlite3.connect(db)
+        self.cursor = self.connection.cursor()
+        self.students = {}
+        self.filename = filename
+        self.target_grade = target_grade
 
-    # outputs a list of Student objects.
-    def extract_students(_in, cursor):
+    def extract_students(self, _in):
+        #print(f"Extracting students from {self.filename}")
         rows = csv.reader(_in)
         curr_grade = ""
-        output = {}
         for row in rows:
+            #print(row)
             if re.search("Grade Level:.*$", row[0]):
                 curr_grade = re.search("(\d+)", row[0]).groups()[0]
             elif re.search("\d{6}.*$", row[0]):
+                #print("doing the thing")
                 new_student = Student(curr_grade, row[0], row[2], row[6], row[7], row[8], row[9], row[10])
-                cursor = new_student.add_to_db(cursor)
-                output[int(new_student.ID)] = new_student
-        return output, cursor
+                new_student.add_to_db(self.connection)
+                self.students[int(new_student.ID)] = new_student
+                print(f"Added {new_student.name} to the DB...")
 
-    def get_attendance_rates(_students, grade):
+    def get_attendance_rates(self):
         # "comprehend" the list of students as a dataframe
-        students_list = _students.values()
-        #TODO:Fix this. Student ID 303234 may not exist forever.
-        df = pd.DataFrame([student.__dict__.values() for student_id, student in students.items()], columns=students[303234].__dict__.keys())
+        students_list = self.students.values()
+        df = pd.DataFrame([student.__dict__.values() for student_id, student in self.students.items()],
+                          columns=Student("", "", "", "", "", "", "0", "0").__dict__.keys())
         # Specify type where appropriate (for maths later on)
         df[['days_present', 'days_enrolled']] = df[['days_present', 'days_enrolled']].apply(pd.to_numeric)
         # filter down to the specified grade
-        df = df[df['grade'] == grade]
+        df = df[df['grade'] == self.target_grade]
+        print(df)
         # compute the attendance rate
         df['attendance_rate'] = df['days_present']/df['days_enrolled']
-        #print(df)
-        # TODO: it's a bit ugly to pass in an array and get back a DataFrame, no?
+        #print(df) 
         return df
 
-    def get_average_attendance_rate(_students_dataFrame):
+    def get_average_attendance_rate(self, _students_dataFrame):
         df = _students_dataFrame
         return df[['attendance_rate']].mean(axis=0)
 
-    with open(filename, 'r') as incoming:
+    def read(self):
+        with open(self.filename, 'r') as incoming:
 
-        conn = sqlite3.connect('students.db')
-        c = conn.cursor()
-       
-       # read the students out of the csv into an array of Student objects
-        students, c = extract_students(incoming, c)
-        #conn.commit()
-        print(students[0])
-        print(c.fetchone())
+            # read the students out of the csv into an array of Student objects
+            self.extract_students(incoming)
+            print(self.students)
+            
+            #print(self.cursor.fetchone())
+            #self.connection.commit()
+            self.connection.close()
 
-        conn.close()
+            # parsing the students into a dataframe lets us do computations on them more easily
+            students_dataframe = self.get_attendance_rates()
 
-        # parsing the students into a dataframe lets us do computations on them more easily
-        students_dataframe = get_attendance_rates(students, '09')
+            print(students_dataframe)
+            
+            self.average_attendance_rate = self.get_average_attendance_rate(students_dataframe)
 
-        average_attendance_rate = get_average_attendance_rate(students_dataframe)
+            def update_attendance(row):
+                #print(f"{row} was the row.")
+                curr_student = self.students[int(row['ID'])]
+                curr_student.attendance_rate = row['attendance_rate']
+                curr_student.attendance_distance = float(curr_student.attendance_rate - self.average_attendance_rate)
+            
+            #df = students_dataframe[students_dataframe['grade'] == self.target_grade]
+            #print(df)
+            students_dataframe.apply(update_attendance, axis=1)
+            #print(f"{students[319752].attendance_rate} ({students[319752].attendance_distance})")
 
-        def update_attendance(row):
-            #print(f"{row} was the row.")
-            curr_student = students[int(row['ID'])]
-            curr_student.attendance_rate = row['attendance_rate']
-            curr_student.attendance_distance = float(curr_student.attendance_rate - average_attendance_rate)
-
-        df = students_dataframe[students_dataframe['grade'] == '09']
-        #print(df)
-        df.apply(update_attendance, axis=1)
-        #print(f"{students[319752].attendance_rate} ({students[319752].attendance_distance})")
-
-        for student_id, student in students.items():
-            if student.grade == '09':
-                with open(f'_templates/attendance.html', 'r') as template:
-                    with open(f'test_data/{student_id}_attendanceReport.html', 'w') as out:
-                        out.write(student.render(template.read()))
+            for student_id, student in self.students.items():
+                if student.grade == self.target_grade:
+                    with open(f'_templates/attendance.html', 'r') as template:
+                        with open(f'test_data/{student_id}_attendanceReport.html', 'w') as out:
+                            out.write(student.render(template.read()))
