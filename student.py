7633d1ac0d3ff0b504489a0f21361906099b6ecd
James Vorderbruggen <james@bip44.email>
2019-08-05 12:29:25 -0400

    Modularize student.py

--- student.py
+++ student.py
@@ -3,8 +3,12 @@ import jinja2 as jinja
 #from report import Report
 import sqlalchemy
 from sqlalchemy import Column, String, Boolean, Integer
+from sqlalchemy.ext.declarative import declarative_base
 
-class Student:
+def __init__():
+    Base = declarative_base()
+
+class Student(Base):
     
     __tablename__ = "students"
 
@@ -12,30 +16,9 @@ class Student:
     name = Column(String)
     email = Column(String)
     phone = Column(String)
-    contact_by_phone(Boolean)
+    contact_by_phone = Column(Boolean)
 
     def render(self, _template):
         tm = jinja.Template(_template)
         return tm.render(student=self)
-
-    #def get_class_average_attendance(self):
-    #    return abs(self.attendance_distance - self.attendance_rate)
-
-    #def add_to_db(self, connection):
-    #    try:
-    #        print(f"Adding {self.name} to the DB...")
-    #        connection.execute("CREATE TABLE if not exists student \
-    #                (ID integer PRIMARY KEY, name text, email text, phone text, contact_by_phone integer)")
-    #        connection.execute("INSERT INTO student VALUES(?, ?, ?, ?, ?)", 
-    #            (int(self.ID), 
-    #            self.name, 
-    #            self.email,
-    #            self.phone,
-    #            self.contact_by_phone))
-    #        connection.commit()
-    #    except sqlite3.IntegrityError:
-    #        pass
-    
-    #def add_report(self, report):
-    #    self.reports.add(report)
 
